0d5c5c9556fe1381246c4d0e9e566d2ecc2ded2d
OopsAllBeary <oopsallbeary@gmail.com>
2026-07-27 01:28:59 -0400

    added basic js and a container style

---
+++ main.js
@@ -0,0 +1,16 @@
+const init = () => {
+    scanDocumentForNecessaryElements();
+    initEventListeners();
+}
+
+const scanDocumentForNecessaryElements = () => {
+
+}
+
+const initEventListener = () => {
+
+}
+
+window.addEventListener("load", function() {
+    init();
+});
