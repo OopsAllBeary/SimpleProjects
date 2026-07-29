06a15c01b5234bdf6a14af4650e90912d189771e
OopsAllBeary <oopsallbeary@gmail.com>
2026-07-29 01:31:56 -0400

    functionality complete

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
+import { DateTime } from "./node_modules/luxon/build/es6/luxon.mjs";
+
+var birthdate, ageText;
+
+
 const init = () => {
     scanDocumentForNecessaryElements();
     initEventListeners();
 }
 
 const scanDocumentForNecessaryElements = () => {
-
+    birthdate = document.querySelector("#birthdate");
+    ageText = document.querySelector('#ageText');
 }
 
 const initEventListeners = () => {
+   birthdate.addEventListener("input", (ev) => {
+    calculateAge(ev.target.value);
+   })
+}
 
+const calculateAge = (userBirthDate) => {
+    const now = DateTime.now();
+    const compareDate = DateTime.fromISO(userBirthDate);
+
+    const exactAge = now.diff(compareDate, ["years", "months", "days"]).toObject();
+    ageText.innerHTML = "You are " + exactAge.years + " years old";
+    
 }
 
 window.addEventListener("load", function() {
