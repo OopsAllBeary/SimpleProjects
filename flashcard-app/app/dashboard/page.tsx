62068ec3cd620b25af14f2055cd300f1fac7ebf3
OopsAllBeary <50027251+OopsAllBeary@users.noreply.github.com>
2026-08-17 00:48:44 -0400

    ive made a mess of this, i might back up and redo the fetch calls

---
+++ flashcard-app/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+
+import Flashcard from "@/app/dashboard/Flashcard";
+
+export default function Dashboard() {
+  return (
+    <div className="min-h-screen h-screen m-0 bg-stone-100">
+      <div className="flex h-1/5 justify-center items-center mx-auto">
+        <h1 className="text-5xl">Welcome to Flash Card App</h1>
+      </div>
+      <div className="flex flex-col h-3/5 justify-center items-center mx-auto">
+        <Flashcard />
+      </div>
+    </div>
+  );
+}
